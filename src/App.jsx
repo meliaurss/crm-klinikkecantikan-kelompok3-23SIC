68ef95b9e94e32631ede3f6c15ef9b464f0e57bb
kinantiara <kinantiara23si@mahasiswa.pcr.ac.id>
2025-05-23 09:28:04 +0700

    selesai penjualan by Kinantiara

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import MainLayout from "./components/MainLayout";
 import Dashboard from "./pages/Dashboard";
+import SalesManagement from "./pages/SalesManagement";
 
 function App() {
   return(
@@ -8,6 +9,7 @@ function App() {
     <Routes>
       <Route element={<MainLayout/>}>
       <Route path="/" element={<Dashboard/>} />
+      <Route path="penjualan" element={<SalesManagement/>} />
       </Route>
     </Routes>
   )
